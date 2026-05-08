df70d9466334a98cad486e57716913ad7e7b9a8d
Star <setarenajafian@gmail.com>
2026-05-08 17:49:53 +0330

    Add input validation using DataAnnotations for LoginRequest Model

--- BankingSecureApi/Models/LoginRequest.cs
+++ BankingSecureApi/Models/LoginRequest.cs
@@ -1,7 +1,14 @@
-﻿namespace BankingSecureApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingSecureApi.Models;
 
 public class LoginRequest
 {
+    [Required]
+    [MinLength(3)]
     public string Username { get; set; }
+
+    [Required]
+    [MinLength(5)]
     public string Password { get; set; }
 }
